b37e5d8208b9f6a80d169b085d123d666f089770
Anil Shokeen <100keenanil@gmail.com>
2020-11-07 10:31:40 +0530

    Sixth Commit Dated on : 07 Nov 2020
    - Complete Code

--- lib/presentation/features/home_screen/view/homescreen.dart
+++ lib/presentation/features/home_screen/view/homescreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tech_sales/presentation/features/leads_screen/view/leadScreen.dart';
 import 'package:flutter_tech_sales/utils/constants/color_constants.dart';
 import 'package:flutter_tech_sales/utils/functions/convert_to_hex.dart';
+import 'package:flutter_tech_sales/widgets/custom_dialogs.dart';
+import 'package:get/get.dart';
 import 'package:slider_button/slider_button.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,6 +21,7 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   String _status = "check_in";
+
   @override
   void initState() {
     // TODO: implement initState
@@ -33,231 +36,237 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: ColorConstants.backgroundColorBlue,
-        appBar: AppBar(
-          // titleSpacing: 50,
-          backgroundColor: ColorConstants.appBarColor,
-          toolbarHeight: 100,
-          centerTitle: false,
-          title: Text(
-            "Home",
-            style: TextStyle(
-                fontWeight: FontWeight.normal,
-                fontSize: 34,
-                color: Colors.white,
-                fontFamily: "Muli"),
-          ),
-          automaticallyImplyLeading: true,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 25.0, top: 20),
-              child: Column(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      print("Notification");
-                    },
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      // margin: EdgeInsets.only(top: 40, left: 40, right: 40),
-                      decoration: new BoxDecoration(
-                        color: Colors.white,
-                        border: Border.all(color: Colors.black, width: 0.0),
-                        borderRadius: new BorderRadius.all(Radius.circular(70)),
-                      ),
-                      child: Icon(
-                        Icons.notifications_none_outlined,
-                        color: HexColor("#FFCD00"),
-                        size: 30,
-                      ),
-                    ),
-
-                    // Icon(
-                    //   Icons.circle_notifications,
-                    //
-                    //   color: Colors.white,
-                    //   size: 50,
-                    // ),
-                  ),
-                  Text(
-                    "Notification",
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  )
-                ],
+    return WillPopScope(
+        onWillPop: () async {
+          // You can do some work here.
+          // Returning true allows the pop to happen, returning false prevents it.
+          Get.dialog(CustomDialogs().errorDialog("Do uou want to exit?"));
+          return true;
+        },
+        child: Scaffold(
+            backgroundColor: ColorConstants.backgroundColorBlue,
+            appBar: AppBar(
+              // titleSpacing: 50,
+              backgroundColor: ColorConstants.appBarColor,
+              toolbarHeight: 100,
+              centerTitle: false,
+              title: Text(
+                "Home",
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 34,
+                    color: Colors.white,
+                    fontFamily: "Muli"),
               ),
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 10.0, top: 20, bottom: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello , Bhupinder",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.normal,
-                        fontFamily: "Muli"),
-                  ),
-                  Text("Here are today's",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.white.withOpacity(0.7),
-                          fontSize: 15,
-                          fontFamily: "Muli")),
-                  Text("recommended actions for you",
-                      style: TextStyle(
-                          color: Colors.white.withOpacity(0.7),
-                          fontSize: 15,
-                          fontFamily: "Muli")),
-                ],
-              ),
-            ),
-            Expanded(child: userMenuWidget())
-            // ListView.builder(
-            //     itemCount: list.length,
-            //     padding: const EdgeInsets.only(top: 10.0),
-            //     itemExtent: 25.0,
-            //     itemBuilder: (context, index) {})
-          ],
-        ),
-        bottomNavigationBar: Container(
-          height: 60,
-          width: 100,
-          child: _status == "check_in" || _status == "check_out"
-              ? _status == "check_in"
-                  ? SliderButton(
-                      action: () {
-                        setState(() {
-                          _status = "check_out";
-                        });
-
-                        ///Do something here OnSlide
-                      },
-
-                      ///Put label over here
-                      label: Text(
-                        "Slide to Check-In !",
-                        style: TextStyle(
-                            color: Color(0xff4a4a4a),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 17),
-                      ),
-                      icon: Center(
+              automaticallyImplyLeading: true,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 25.0, top: 20),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          print("Notification");
+                        },
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          // margin: EdgeInsets.only(top: 40, left: 40, right: 40),
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black, width: 0.0),
+                            borderRadius:
+                                new BorderRadius.all(Radius.circular(70)),
+                          ),
                           child: Icon(
-                        Icons.arrow_forward_outlined,
-                        color: Colors.white,
-                        size: 40.0,
-                        //  semanticLabel: 'Text to announce in accessibility modes',
-                      )),
+                            Icons.notifications_none_outlined,
+                            color: HexColor("#FFCD00"),
+                            size: 30,
+                          ),
+                        ),
 
-                      ///Change All the color and size from here.
-                      alignLabel: Alignment.center,
-                      width: MediaQuery.of(context).size.width,
-                      radius: 10,
-                      buttonColor: ColorConstants.checkinColor,
-                      backgroundColor: ColorConstants.checkinColor,
-                      highlightedColor: Colors.grey,
-                      baseColor: Colors.white,
-                      vibrationFlag: true,
-                     dismissible: false,
-                    )
+                        // Icon(
+                        //   Icons.circle_notifications,
+                        //
+                        //   color: Colors.white,
+                        //   size: 50,
+                        // ),
+                      ),
+                      Text(
+                        "Notification",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0, top: 20, bottom: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Hello , Bhupinder",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.normal,
+                            fontFamily: "Muli"),
+                      ),
+                      Text("Here are today's",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 15,
+                              fontFamily: "Muli")),
+                      Text("recommended actions for you",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 15,
+                              fontFamily: "Muli")),
+                    ],
+                  ),
+                ),
+                Expanded(child: userMenuWidget())
+                // ListView.builder(
+                //     itemCount: list.length,
+                //     padding: const EdgeInsets.only(top: 10.0),
+                //     itemExtent: 25.0,
+                //     itemBuilder: (context, index) {})
+              ],
+            ),
+            bottomNavigationBar: Container(
+              height: 60,
+              width: 100,
+              child: _status == "check_in" || _status == "check_out"
+                  ? _status == "check_in"
+                      ? SliderButton(
+                          action: () {
+                            setState(() {
+                              _status = "check_out";
+                            });
+
+                            ///Do something here OnSlide
+                          },
+
+                          ///Put label over here
+                          label: Text(
+                            "Slide to Check-In !",
+                            style: TextStyle(
+                                color: Color(0xff4a4a4a),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17),
+                          ),
+                          icon: Center(
+                              child: Icon(
+                            Icons.arrow_forward_outlined,
+                            color: Colors.white,
+                            size: 40.0,
+                            //  semanticLabel: 'Text to announce in accessibility modes',
+                          )),
+
+                          ///Change All the color and size from here.
+                          alignLabel: Alignment.center,
+                          width: MediaQuery.of(context).size.width,
+                          radius: 10,
+                          buttonColor: ColorConstants.checkinColor,
+                          backgroundColor: ColorConstants.checkinColor,
+                          highlightedColor: Colors.grey,
+                          baseColor: Colors.white,
+                          vibrationFlag: true,
+                          dismissible: false,
+                        )
+                      // GestureDetector(
+                      //             onTap: () {
+                      //               setState(() {
+                      //                 _status = "check_out";
+                      //               });
+                      //             },
+                      //             child: Container(
+                      //               alignment: Alignment.center,
+                      //               color: ColorConstants.checkinColor,
+                      //               child: Text("CHECK-IN",
+                      //                   style: TextStyle(
+                      //                       color: Colors.white,
+                      //                       fontFamily: "Muli",
+                      //                       fontSize: 18)),
+                      //             ),
+                      //           )
+                      : SliderButton(
+                          action: () {
+                            setState(() {
+                              _status = "Journey_Ended";
+                            });
+
+                            ///Do something here OnSlide
+                          },
+
+                          ///Put label over here
+                          label: Text(
+                            "Slide to Check-Out !",
+                            style: TextStyle(
+                                color: Color(0xff4a4a4a),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 17),
+                          ),
+                          icon: Center(
+                              child: Icon(
+                            Icons.arrow_forward_outlined,
+                            color: Colors.white,
+                            size: 40.0,
+                            //  semanticLabel: 'Text to announce in accessibility modes',
+                          )),
+
+                          ///Change All the color and size from here.
+                          alignLabel: Alignment.center,
+                          width: MediaQuery.of(context).size.width,
+                          radius: 10,
+                          buttonColor: HexColor("#F9A61A"),
+                          backgroundColor: HexColor("#F9A61A"),
+                          highlightedColor: Colors.grey,
+                          baseColor: Colors.white,
+                          vibrationFlag: true,
+                          dismissible: false,
+                        )
                   // GestureDetector(
                   //             onTap: () {
                   //               setState(() {
-                  //                 _status = "check_out";
+                  //                 _status = "Journey_Ended";
                   //               });
                   //             },
                   //             child: Container(
                   //               alignment: Alignment.center,
-                  //               color: ColorConstants.checkinColor,
-                  //               child: Text("CHECK-IN",
+                  //               color: Colors.red,
+                  //               child: Text("CHECK-OUT",
                   //                   style: TextStyle(
                   //                       color: Colors.white,
                   //                       fontFamily: "Muli",
                   //                       fontSize: 18)),
                   //             ),
                   //           )
-                  :
-          SliderButton(
-            action: () {
-              setState(() {
-                _status = "Journey_Ended";
-              });
-
-              ///Do something here OnSlide
-            },
-
-            ///Put label over here
-            label: Text(
-              "Slide to Check-Out !",
-              style: TextStyle(
-                  color: Color(0xff4a4a4a),
-                  fontWeight: FontWeight.w500,
-                  fontSize: 17),
-            ),
-            icon: Center(
-                child: Icon(
-                  Icons.arrow_forward_outlined,
-                  color: Colors.white,
-                  size: 40.0,
-                  //  semanticLabel: 'Text to announce in accessibility modes',
-                )),
-
-            ///Change All the color and size from here.
-            alignLabel: Alignment.center,
-            width: MediaQuery.of(context).size.width,
-            radius: 10,
-            buttonColor: HexColor("#F9A61A"),
-            backgroundColor: HexColor("#F9A61A"),
-            highlightedColor: Colors.grey,
-            baseColor: Colors.white,
-            vibrationFlag: true,
-            dismissible: false,
-          )
-          // GestureDetector(
-          //             onTap: () {
-          //               setState(() {
-          //                 _status = "Journey_Ended";
-          //               });
-          //             },
-          //             child: Container(
-          //               alignment: Alignment.center,
-          //               color: Colors.red,
-          //               child: Text("CHECK-OUT",
-          //                   style: TextStyle(
-          //                       color: Colors.white,
-          //                       fontFamily: "Muli",
-          //                       fontSize: 18)),
-          //             ),
-          //           )
-              :
-          GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _status = "check_in";
-                    });
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.grey,
-                    child: Text("Journey-Ended",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "Muli",
-                            fontSize: 18)),
-                  ),
-                ),
-        ));
+                  : GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _status = "check_in";
+                        });
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.grey,
+                        child: Text("Journey-Ended",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: "Muli",
+                                fontSize: 18)),
+                      ),
+                    ),
+            )));
   }
 
   Widget userMenuWidget() {
