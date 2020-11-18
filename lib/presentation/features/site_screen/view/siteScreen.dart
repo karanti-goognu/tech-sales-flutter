da21adfab514ead8f834a5cd23e94583ed3309c5
sumit <sumitk@futuresoft.in>
2020-11-18 11:26:29 +0530

    Merge branches 'Feature_Dhawan' and 'master' of https://gitlab.com/dalmia_digital/tech-sales-flutter into Feature_Dhawan
     Conflicts:
    	lib/bindings/leads__filter_binding.dart
    	lib/presentation/features/leads_screen/view/leadScreen.dart
    	lib/routes/app_pages.dart

---
+++ lib/presentation/features/site_screen/view/siteScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class SiteScreen extends StatefulWidget {
+  @override
+  _SiteScreenState createState() => _SiteScreenState();
+}
+
+class _SiteScreenState extends State<SiteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
