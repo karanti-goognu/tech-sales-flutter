38596df9b50b40887e9bde929a5f1591923c6c18
Sunpreet Singh <sunpreets@futuresoft.in>
2021-01-25 13:40:11 +0530

    code commit

--- lib/presentation/features/service_requests/controller/save_service_request_controller.dart
+++ lib/presentation/features/service_requests/controller/save_service_request_controller.dart
@@ -8,8 +8,9 @@ import 'package:flutter_tech_sales/routes/app_pages.dart';
 import 'package:flutter_tech_sales/utils/constants/string_constants.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_tech_sales/widgets/custom_dialogs.dart';
 
-class SaveServiceRequestController extends GetxController{
+class SaveServiceRequestController extends GetxController {
   @override
   void onInit() {
     super.onInit();
@@ -17,58 +18,69 @@ class SaveServiceRequestController extends GetxController{
 
   final SrRepository repository;
 
-  SaveServiceRequestController({@required this.repository}) : assert(repository != null);
+  SaveServiceRequestController({@required this.repository})
+      : assert(repository != null);
   final _saveRequestData = SrComplaintModel().obs;
   get saveRequestData => _saveRequestData.value;
-  set saveRequestData(value) =>
-      _saveRequestData.value = value;
-  String responseForDialog='';
-
+  set saveRequestData(value) => _saveRequestData.value = value;
+  String responseForDialog = '';
 
   List<File> imageList;
 
-
-  Future<AccessKeyModel> getAccessKey(){
+  Future<AccessKeyModel> getAccessKey() {
     return repository.getAccessKey();
   }
 
-  getAccessKeyAndSaveRequest(List<File> imageList, SaveServiceRequest saveRequestModel) {
+  getAccessKeyAndSaveRequest(
+      List<File> imageList, SaveServiceRequest saveRequestModel) {
     String userSecurityKey = "";
     String empID = "";
     Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
     Future.delayed(
         Duration.zero,
-            () => Get.dialog(Center(child: CircularProgressIndicator()),
+        () => Get.dialog(Center(child: CircularProgressIndicator()),
             barrierDismissible: false));
-    repository.getAccessKey().then((data) async{
+    repository.getAccessKey().then((data) async {
       // Get.back();
 
       await _prefs.then((SharedPreferences prefs) async {
         userSecurityKey = prefs.getString(StringConstants.userSecurityKey);
-        saveServiceRequest(imageList, data.accessKey, userSecurityKey, saveRequestModel).then((value){
+        saveServiceRequest(
+                imageList, data.accessKey, userSecurityKey, saveRequestModel)
+            .then((value) {
           print(value);
           Get.back();
-         if(value['resp-code']=='SRC2035'){
-           Get.defaultDialog(title:"Message",
-             middleText: value['resp-msg'].toString(),
-             confirm: MaterialButton(onPressed: ()=>Get.back(),child: Text('OK'),),
-               barrierDismissible: false
-           );
-         } else{
-           Get.defaultDialog(title:"Message",
-             middleText: value['resp-msg'].toString(),
-             // confirm: MaterialButton(onPressed: ()=>Get.back(),child: Text('OK'),),
-             confirm: MaterialButton(onPressed: ()=>Get.toNamed(Routes.HOME_SCREEN),child: Text('OK'),),
-               barrierDismissible: false
-           );
-         }
+          if (value['resp-code'] == 'SRC2035') {
+            Get.defaultDialog(
+                title: "Message",
+                middleText: value['resp-msg'].toString(),
+                confirm: MaterialButton(
+                  onPressed: () => Get.back(),
+                  child: Text('OK'),
+                ),
+                barrierDismissible: false);
+          } else {
+            Get.dialog(
+                CustomDialogs().messageDialogMWP(value['resp-msg'].toString()),
+                barrierDismissible: false);
+            // Get.defaultDialog(title:"Message",
+            //   middleText: value['resp-msg'].toString(),
+            //   // confirm: MaterialButton(onPressed: ()=>Get.back(),child: Text('OK'),),
+            //   confirm: MaterialButton(onPressed: ()=>Get.toNamed(Routes.HOME_SCREEN),child: Text('OK'),),
+            //     barrierDismissible: false
+            // );
+          }
         });
       });
     });
   }
 
-  Future<Map> saveServiceRequest(List<File> imageList, String accessKey, String userSecurityKey,SaveServiceRequest saveRequestModel){
-    return repository.saveServiceRequest(imageList, accessKey, userSecurityKey, saveRequestModel).whenComplete(() => responseForDialog ='Test');
+  Future<Map> saveServiceRequest(List<File> imageList, String accessKey,
+      String userSecurityKey, SaveServiceRequest saveRequestModel) {
+    return repository
+        .saveServiceRequest(
+            imageList, accessKey, userSecurityKey, saveRequestModel)
+        .whenComplete(() => responseForDialog = 'Test');
   }
 }
