62a4a891bd8de2ebfda41d2b56c633e81521f26b
jilu <jilusubudhi@gmail.com>
2021-02-18 12:35:29 +0530

    maps api key updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,7 +14,7 @@ import MoEngage
   MoEngage.setAppGroupID("group.com.alphadevs.MoEngage.NotificationServices")
   MOFlutterInitializer.sharedInstance.initializeWithAppID("6XHHUKUOKFE6Q5MLTT8UH5RW", withLaunchOptions: launchOptions)
   FirebaseApp.configure()
-  GMSServices.provideAPIKey("AIzaSyBMMbdXDz2Vt9O8hF8l4xNGRQAwHdMegZ4")
+  GMSServices.provideAPIKey("AIzaSyC8GcSwDzPq7gW_vKmsTNv9Xqr9WvwfA5E")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
