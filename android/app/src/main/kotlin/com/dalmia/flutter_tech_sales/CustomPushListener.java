4ae92727264a8978f32b6dc6e7b51cbef58a9714
Shivani Tyagi <@shivanit678>
2021-03-10 10:24:18 +0530

    work on store data in local db

--- android/app/src/main/kotlin/com/dalmia/flutter_tech_sales/CustomPushListener.java
+++ android/app/src/main/kotlin/com/dalmia/flutter_tech_sales/CustomPushListener.java
@@ -4,23 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import com.moengage.core.Logger;
-import com.moengage.plugin.base.PluginPushCallback;
+//import com.moengage.plugin.base.PluginPushCallback;
 
 /**
  * @author Shailesh Diwakar
  * Date: 2021/02/16
  */
-public class CustomPushListener extends PluginPushCallback {
+public class CustomPushListener /*extends PluginPushCallback*/ {
 
-  private static final String TAG = "CustomPushListener";
-
-  @Override public void onHandleRedirection(Activity activity, Bundle payload) {
-    super.onHandleRedirection(activity, payload);
-    Logger.v(TAG + " onHandleRedirection() : ");
-  }
-
-  @Override public void onNotificationReceived(Context context, Bundle payload) {
-    super.onNotificationReceived(context, payload);
-    Logger.v(TAG + " onNotificationReceived() : ");
-  }
+//  private static final String TAG = "CustomPushListener";
+//
+//  @Override public void onHandleRedirection(Activity activity, Bundle payload) {
+//    super.onHandleRedirection(activity, payload);
+//    Logger.v(TAG + " onHandleRedirection() : ");
+//  }
+//
+//  @Override public void onNotificationReceived(Context context, Bundle payload) {
+//    super.onNotificationReceived(context, payload);
+//    Logger.v(TAG + " onNotificationReceived() : ");
+//  }
 }
--- android/app/src/main/kotlin/com/dalmia/flutter_tech_sales/MainApplication.java
+++ android/app/src/main/kotlin/com/dalmia/flutter_tech_sales/MainApplication.java
@@ -2,10 +2,10 @@ package com.dalmia.flutter_tech_sales;
 
 
 import com.moengage.core.LogLevel;
-import com.moengage.core.MoEngage;
-import com.moengage.core.MoEngage.Builder;
-import com.moengage.flutter.MoEInitializer;
-import com.moengage.pushbase.MoEPushHelper;
+//import com.moengage.core.MoEngage;
+//import com.moengage.core.MoEngage.Builder;
+//import com.moengage.flutter.MoEInitializer;
+//import com.moengage.pushbase.MoEPushHelper;
 import io.flutter.app.FlutterApplication;
 
 public class MainApplication extends FlutterApplication {
@@ -13,16 +13,16 @@ public class MainApplication extends FlutterApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        MoEngage.Builder builder = new Builder(this, "USWINCHCY9D2ZRV2XSAZBC0M")
-                .setNotificationSmallIcon(R.mipmap.ic_launcher)
-                .setNotificationLargeIcon(R.mipmap.ic_launcher)
-                .optOutDefaultInAppDisplay()
-                .enableLogs(LogLevel.VERBOSE)
-                .enablePushKitTokenRegistration();
-
-        MoEInitializer.initialize(getApplicationContext(), builder);
-        // optional, required in-case notification customisation is required.
-        MoEPushHelper.getInstance().setMessageListener(new CustomPushListener());
+//        MoEngage.Builder builder = new Builder(this, "USWINCHCY9D2ZRV2XSAZBC0M")
+//                .setNotificationSmallIcon(R.mipmap.ic_launcher)
+//                .setNotificationLargeIcon(R.mipmap.ic_launcher)
+//                .optOutDefaultInAppDisplay()
+//                .enableLogs(LogLevel.VERBOSE)
+//                .enablePushKitTokenRegistration();
+//
+//        MoEInitializer.initialize(getApplicationContext(), builder);
+//        // optional, required in-case notification customisation is required.
+//        MoEPushHelper.getInstance().setMessageListener(new CustomPushListener());
     }
 }
 
