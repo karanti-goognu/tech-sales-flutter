8de7ad83412e8c45bb017f8f5470a83a7c1a6af4
unknown <hp@Singh_sunpreet.dalmiabharat.com>
2021-04-01 23:24:38 +0530

    mwp issue fixed

--- lib/presentation/features/mwp/view/add_mwp.dart
+++ lib/presentation/features/mwp/view/add_mwp.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
-import 'package:connectivity/connectivity.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tech_sales/core/data/controller/app_controller.dart';
-import 'package:flutter_tech_sales/presentation/features/leads_screen/view/DraftLeadListScreen.dart';
 import 'package:flutter_tech_sales/presentation/features/mwp/controller/mwp_plan_controller.dart';
 import 'package:flutter_tech_sales/presentation/features/mwp/view/add_mwp_plan_view.dart';
-import 'package:flutter_tech_sales/routes/app_pages.dart';
 import 'package:flutter_tech_sales/utils/constants/color_constants.dart';
 import 'package:flutter_tech_sales/utils/constants/request_ids.dart';
 import 'package:flutter_tech_sales/utils/size/size_config.dart';
 import 'package:get/get.dart';
+import 'package:flutter_tech_sales/widgets/bottom_navigator.dart';
+import 'package:flutter_tech_sales/widgets/customFloatingButton.dart';
+
+
 import 'package:intl/intl.dart';
 
 class AddMWP extends StatefulWidget {
@@ -25,7 +26,7 @@ class AddMWPScreenPageState extends State<AddMWP> {
   AppController _appController = Get.find();
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_){
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       print(json.encode(_mwpPlanController.getMWPResponse));
       print(1);
       final DateTime now = DateTime.now();
@@ -38,7 +39,6 @@ class AddMWPScreenPageState extends State<AddMWP> {
 
     super.initState();
   }
-
 
   @override
   void dispose() {
@@ -55,114 +55,10 @@ class AddMWPScreenPageState extends State<AddMWP> {
       floatingActionButton: Container(
         height: 68.0,
         width: 68.0,
-        child: FittedBox(
-          child: FloatingActionButton(
-            backgroundColor: ColorConstants.checkinColor,
-            child: Icon(
-              Icons.keyboard_backspace,
-              color: Colors.black,
-            ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ),
+        child: BackFloatingButton(),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar:
-      BottomAppBar(
-        color: ColorConstants.appBarColor,
-        shape: CircularNotchedRectangle(),
-        notchMargin: 10,
-        child: Container(
-          height: 60,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      Get.toNamed(Routes.HOME_SCREEN);
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.home,
-                          color: Colors.white60,
-                        ),
-                        Text(
-                          'Home',
-                          style: TextStyle(
-                            color: Colors.white60,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-
-              // Right Tab bar icons
-
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          new CupertinoPageRoute(
-                              builder: (BuildContext context) =>
-                                  DraftLeadListScreen()));
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.drafts,
-                          color: Colors.white60,
-                        ),
-                        Text(
-                          'Drafts',
-                          style: TextStyle(
-                            color: Colors.white60,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      Get.toNamed(Routes.SEARCH_SCREEN);
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.search,
-                          color: Colors.white60,
-                        ),
-                        Text(
-                          'Search',
-                          style: TextStyle(
-                            color: Colors.white60,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+      bottomNavigationBar: BottomNavigator(),
     );
   }
 
@@ -191,58 +87,59 @@ class AddMWPScreenPageState extends State<AddMWP> {
                       ),
                     ),
                     Obx(
-                      () =>
+                          () =>
                       (_mwpPlanController
-                                  .getMWPResponse.listOfMonthYear !=
-                              null) ?
+                          .getMWPResponse.listOfMonthYear !=
+                          null) ?
                       Flexible(
-                              flex: 2,
-                              child: Container(
-                                  width: double.infinity,
-                                  padding:
-                                      const EdgeInsets.fromLTRB(12, 4, 4, 4),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      new BoxShadow(
-                                        color: Colors.grey,
-                                        blurRadius: 8.0,
-                                      ),
-                                    ],
-                                  ),
-                                  child: DropdownButtonHideUnderline(
-                                    child: Obx(
-                                      () => DropdownButton<String>(
-                                        value: _mwpPlanController.selectedMonth,
-                                        onChanged: (String newValue) {
-                                          print("Month--->"+'$newValue');
-                                          _mwpPlanController.selectedMonth =
-                                              newValue;
-                                          _appController.getAccessKey(
-                                              RequestIds.GET_MWP_PLAN);
-                                          _mwpPlanController.isLoading = true;
-                                        },
-                                        items: _mwpPlanController
-                                            .getMWPResponse.listOfMonthYear
-                                            .map<DropdownMenuItem<String>>(
-                                                (String value) {
-                                          return DropdownMenuItem<String>(
-                                            value: value,
-                                            child: Text(
-                                              value, style: TextStyle(
-                                                  fontSize: SizeConfig.safeBlockHorizontal*.1,
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          );
-                                        }).toList(),
-                                      ),
-                                    ),
-                                  )),
-                            )
-                          : Container(
-                              child: Text("Error"),
+                        flex: 2,
+                        child: Container(
+                            width: double.infinity,
+                            padding:
+                            const EdgeInsets.fromLTRB(12, 4, 4, 4),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4),
+                              color: Colors.white,
+                              boxShadow: [
+                                new BoxShadow(
+                                  color: Colors.grey,
+                                  blurRadius: 8.0,
+                                ),
+                              ],
                             ),
+                            child: DropdownButtonHideUnderline(
+                              child: Obx(
+                                    () => DropdownButton<String>(
+                                  value: _mwpPlanController.selectedMonth,
+                                  onChanged: (String newValue) {
+                                    print("Month--->"+'$newValue');
+                                    _mwpPlanController.selectedMonth =
+                                        newValue;
+                                    _appController.getAccessKey(
+                                        RequestIds.GET_MWP_PLAN);
+                                    _mwpPlanController.isLoading = true;
+                                  },
+                                  items: _mwpPlanController
+                                      .getMWPResponse.listOfMonthYear
+                                      .map<DropdownMenuItem<String>>(
+                                          (String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(
+                                            value,
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        );
+                                      }).toList(),
+                                ),
+                              ),
+                            )),
+                      )
+                          : Container(
+                        child: Text("Error"),
+                      ),
                     ),
                   ],
                 ),
@@ -256,3 +153,5 @@ class AddMWPScreenPageState extends State<AddMWP> {
     );
   }
 }
+
+
