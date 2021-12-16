13aafc7b89823d1bb59c15352b740818e53a3852
Pallavi Fegade <pallavi.fegade@neosoftmail.com>
2021-12-16 11:45:56 +0530

    api end point added to url constant

--- lib/utils/constants/url_constants.dart
+++ lib/utils/constants/url_constants.dart
@@ -15,14 +15,14 @@ abstract class UrlConstants {
 
  // QA
  //
- // static const String baseUrl = 'https://mobileqacloud.dalmiabharat.com/tech_sales_server';
- // static const String baseUrlforImages = 'https://mobileqacloud.dalmiabharat.com/tso/leads';
- // static const String baseUrlforImagesSites = 'https://mobileqacloud.dalmiabharat.com/tso/site';
+ static const String baseUrl = 'https://mobileqacloud.dalmiabharat.com/tech_sales_server';
+ static const String baseUrlforImages = 'https://mobileqacloud.dalmiabharat.com/tso/leads';
+ static const String baseUrlforImagesSites = 'https://mobileqacloud.dalmiabharat.com/tso/site';
 
  //Development
- static const String baseUrl = 'https://mobiledevcloud.dalmiabharat.com/tech_sales_server';
- static const String baseUrlforImages = 'https://mobiledevcloud.dalmiabharat.com/tso/leads';
- static const String baseUrlforImagesSites = 'https://mobiledevcloud.dalmiabharat.com/tso/sites';
+ // static const String baseUrl = 'https://mobiledevcloud.dalmiabharat.com/tech_sales_server';
+ // static const String baseUrlforImages = 'https://mobiledevcloud.dalmiabharat.com/tso/leads';
+ // static const String baseUrlforImagesSites = 'https://mobiledevcloud.dalmiabharat.com/tso/sites';
 
 
  //End points
@@ -78,6 +78,8 @@ abstract class UrlConstants {
  static const String updateVersion4SiteData = '$baseUrl/sites/v4/site-update';
  static const String getLeadData2 = '$baseUrl/leads/v2/view-lead?leadId=';
 
+ static const String siteDistList = '$baseUrl/sites/district-list';
+
 
  static const String saveUpdateSiteVisit = '$baseUrl/mwp/Save-update-site_visit';
 
@@ -100,6 +102,7 @@ abstract class UrlConstants {
  static const String getInfDetails = '$baseUrl/influencer/get-influencer-detail?inflContact=';
  static const String saveInfluencer = '$baseUrl/influencer/save-influencer';
  static const String submitEndEvent = '$baseUrl/event/end_event';
+
 
 
  // pending supply
