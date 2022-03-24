2edced98e87eb9c21bb52e88f7230f635334f045
Pallavi Fegade <pallavi.fegade@neosoftmail.com>
2022-03-24 18:55:29 +0530

    url change to prod

--- lib/utils/constants/url_constants.dart
+++ lib/utils/constants/url_constants.dart
@@ -7,17 +7,15 @@ abstract class UrlConstants {
  UrlConstants._();
 
  //PROD
- // static const String baseUrl = 'https://mobileapps.dalmiabharat.com/tech_sales_server';
- // static const String baseUrlforImages = 'https://mobileapps.dalmiabharat.com/tso/leads';
- // static const String baseUrlforImagesSites = 'https://mobileapps.dalmiabharat.com/tso/sites';
- //Base Url
- // static const String baseUrl = 'https://mobileapps.dalmiabharat.com/tech-sales-server';
+ static const String baseUrl = 'https://mobileapps.dalmiabharat.com/tech_sales_server';
+ static const String baseUrlforImages = 'https://mobileapps.dalmiabharat.com/tso/leads';
+ static const String baseUrlforImagesSites = 'https://mobileapps.dalmiabharat.com/tso/sites';
 
 
  // QA
- static const String baseUrl = 'https://mobileqacloud.dalmiabharat.com/tech_sales_server';
- static const String baseUrlforImages = 'https://mobileqacloud.dalmiabharat.com/tso/leads';
- static const String baseUrlforImagesSites = 'https://mobileqacloud.dalmiabharat.com/tso/site';
+ // static const String baseUrl = 'https://mobileqacloud.dalmiabharat.com/tech_sales_server';
+ // static const String baseUrlforImages = 'https://mobileqacloud.dalmiabharat.com/tso/leads';
+ // static const String baseUrlforImagesSites = 'https://mobileqacloud.dalmiabharat.com/tso/site';
 
  //Development
  // static const String baseUrl = 'https://mobiledevcloud.dalmiabharat.com/tech_sales_server';
